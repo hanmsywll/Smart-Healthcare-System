61538b25b7769803b224c46cc4b43fcd01ee22b6
Raihan Syawal <raihansyawal7@gmail.com>
2025-11-10 11:07:39 +0700

    fix swagger

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-Route::get('/', function () {
-    return redirect()->to('/api/documentation');
-});
+use App\Http\Controllers\Web\HomeController;
+
+Route::get('/', [HomeController::class, 'index']);
